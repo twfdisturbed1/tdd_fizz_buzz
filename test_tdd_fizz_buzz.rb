fa6a258a72911fedbbe2a60a3e519c9de45139b7
William Mullins <twfdisturbed@hotmail.com>
2018-06-20 14:07:10 -0400

    test_assert_that_third_postion_is_mined

--- test_tdd_fizz_buzz.rb
+++ test_tdd_fizz_buzz.rb
@@ -12,4 +12,11 @@ class TestYourFunctions < Minitest::Test
     def test_assert_array_length_is_100
     assert_equal(100, fizzbuzz().length)
     end
+    def assert_equal_first_postion_is_1
+        assert_equal(1, fizzbuzz[].first)
+    end
+    def test_assert_that_third_postion_is_mined
+        assert_equal("mined", 2)
+
+    end
 end
