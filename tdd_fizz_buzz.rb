c73f574f4813a36908d22310ec4664cbc8e3e059
William Mullins <twfdisturbed@hotmail.com>
2018-06-20 15:50:41 -0400

    fixed

--- tdd_fizz_buzz.rb
+++ tdd_fizz_buzz.rb
@@ -1,7 +1,15 @@
 def fizzbuzz()
-fizzarr = [*1..100]
-fizzarr[2] = "mined"
-fizzarr[4] = "minds"
-fizzarr[14] = "mined minds"
-fizzarr
+	fizzarr = [*1..100]
+	new_arr = []
+	fizzarr.each do |item|
+		if item %3 == 0 && item %5 == 0
+			item = "mined minds"
+        elsif item %5 == 0
+            item = "minds"
+        elsif item %3 == 0
+            item = "mined"
+        end
+		new_arr << item
+	end
+	new_arr
 end
--- test_tdd_fizz_buzz.rb
+++ test_tdd_fizz_buzz.rb
@@ -1,30 +1,40 @@
 require "minitest/autorun"
 require_relative "tdd_fizz_buzz.rb"
 
-class TestYourFunctions < Minitest::Test
+class TestTddFizzBuzz < Minitest::Test
 
-    def test_assert_that_1_equals_1
+    def test_assert_that_1_equals_1()
         assert_equal(1, 1)
     end
-    def test_assert_that_end_result_is_array
-        assert_equal(Array, fizzbuzz().class)
-    end
-    def test_assert_array_length_is_100
-    assert_equal(100, fizzbuzz().length)
-    end
-    def assert_equal_first_postion_is_1
-        assert_equal(1, fizzbuzz[].first)
-    end
-    def test_assert_that_third_postion_is_mined
-        assert_equal("mined", fizzbuzz[2])
 
+    def test_assert_that_end_result_is_array()
+    	assert_equal(Array, fizzbuzz().class)
     end
-    def test_assert_that_fith_postion_is_mines
-        assert_equal("minds", fizzbuzz[4])
 
+    def test_assert_array_length_is_100()
+    	assert_equal(100, fizzbuzz().length)
     end
-    def test_assert_that_15th_postion_is_mined_mines
-        assert_equal("mined minds", fizzbuzz[14])
 
+    def test_assert_first_position_is_1()
+    	assert_equal(1, fizzbuzz().first)
     end
+
+    def test_assert_third_position_is_mined()
+    	assert_equal("mined", fizzbuzz()[2])
+    end
+
+	def test_assert_fifth_position_is_minds()
+		assert_equal("minds", fizzbuzz()[4])
+	end
+
+	def test_assert_fifteenth_position_is_mined_minds()
+		assert_equal("mined minds", fizzbuzz()[14])
+	end
+
+	def test_assert_multiples_of_3_returns_mined()
+		assert_equal("mined", fizzbuzz()[5])
+		assert_equal("mined", fizzbuzz()[32])
+	end
+
+
 end
