743f75227fff83400c61bb91ea6df3e71c75f691
William Mullins <twfdisturbed@hotmail.com>
2018-06-20 13:49:56 -0400

    Test_assert_that_end_result_is_array

--- test_tdd_fizz_buzz.rb
+++ test_tdd_fizz_buzz.rb
@@ -1,10 +1,11 @@
 require "minitest/autorun"
-require_relative "your_file.rb"
+require_relative "tdd_fizz_buzz.rb"
 
 class TestYourFunctions < Minitest::Test
 
     def test_assert_that_1_equals_1
         assert_equal(1, 1)
     end
-
+    def test_assert_that_end_result_is_array
+        assert_equal(Array, fizzbuzz().class)
 end
